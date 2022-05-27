0ae326fa38e2ded6c98833fe2da3a12bb8168d2c
scrooge365 <lukastrade@seznam.cz>
2022-05-27 12:32:14 +0200

    feat: accessibility

--- components/Accordion/AccordionPanel.js
+++ components/Accordion/AccordionPanel.js
@@ -1,24 +1,35 @@
-import { useState } from 'react';
-import IconArrow from '../../assets/icon-arrow-up.svg'
+import { useState } from "react";
+import IconArrow from "../../assets/icon-arrow-up.svg";
 
 export default function AccordionPanel({ title, text }) {
   const [isOpen, setIsOpen] = useState(false);
-  const handleMouseDown = () => setIsOpen(!isOpen)
+  const handleMouseDown = () => setIsOpen(!isOpen);
+  const handleKeyDown = (e) => (e.key === "Enter" ? setIsOpen(!isOpen) : null);
 
   return (
-    <div className={`a-accordion__item ${isOpen ? 'a-accordion__item--open' : ''}`}>
-      <div className="a-accordion__item-content">
-        {text}
-      </div>
-      <header
+    <div
+      className={`a-accordion__item ${isOpen ? "a-accordion__item--open" : ""}`}
+    >
+      <div
+        id="accordion" // id is important for connection between aria-labelledby
         className="a-accordion__item-header"
-        aria-expanded={isOpen ? 'true' : 'false'}
-        aria-selected={isOpen ? 'true' : 'false'}
+        aria-controls="accordion_content" // identifies the element with content what is controlled by this "button"
+        aria-expanded={isOpen ? true : false} // NVDA is inform with any change
         onMouseDown={handleMouseDown}
+        onKeyDown={(e) => handleKeyDown(e)} // This event is for keyboard accessibility
+        tabIndex={0} // for keyboard visibility focus
+        role="button" // set the div as button for NVDA
       >
         <p className="a-accordion__item-text">{title}</p>
-        <IconArrow className="a-accordion__item-icon"/>
-      </header>
+        <IconArrow className="a-accordion__item-icon" />
+      </div>
+      <div
+        className="a-accordion__item-content"
+        aria-labelledby="accordion"
+        id="accordion_content"
+      >
+        {text}
+      </div>
     </div>
-  )
+  );
 }
